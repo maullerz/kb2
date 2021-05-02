af8023f86a391711124e58cc5a74412a76cbf095
maullerz <maullerz@gmail.com>
2021-05-02 18:00:42 +0300

    Cleanup

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
-import Icon from './Icon'
 import Table from './Table'
 import NoContent from './NoContent'
 import Spinner from './Spinner'
 import Href from './Href'
 
 export {
-  Icon,
   Table,
   NoContent,
   Spinner,
