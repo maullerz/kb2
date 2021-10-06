8b275f26a2853b63510e35b9fd540e4bddb1e878
maullerz <maullerz@gmail.com>
2021-10-06 21:15:10 +0300

    Leave only 2 slots for ads

--- src/components/Ads/index.js
+++ src/components/Ads/index.js
@@ -7,14 +7,14 @@ const LIST_SLOT = '9463018630'
 const SLOTS = {
   list: '9463018630',
   killmail: '9454445621',
-  alliance: '5131879624',
-  corp: '7797022673',
-  char: '4145745796',
-  ship: '2832664125',
-  preset: '9398072471',
-  group: '1938740075',
-  system: '2057610831',
-  region: '4013883850',
+  // alliance: '5131879624',
+  // corp: '7797022673',
+  // char: '4145745796',
+  // ship: '2832664125',
+  // preset: '9398072471',
+  // group: '1938740075',
+  // system: '2057610831',
+  // region: '4013883850',
 }
 
 const responsiveStyle = { display: 'inline-block' }
