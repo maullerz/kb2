827a2e34be5109b2cdcb3dd8fe617bbde4c3b928
maullerz <maullerz@gmail.com>
2021-12-01 01:53:58 +0300

    fixing webpack build

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -72,26 +72,27 @@ module.exports = {
     moduleIds: 'deterministic',
     chunkIds: 'deterministic',
     // runtimeChunk: 'single',
+    sideEffects: false,
 
-    splitChunks: {
-      // include all types of chunks
-      chunks: 'all',
-      // chunks: 'async',
-      cacheGroups: {
-        defaultVendors: {
-          // test: /[\\/]node_modules[\\/]/,
-          // reuseExistingChunk: true,
-          priority: -10,
-          test: /[\\/]node_modules[\\/]/,
-          // name: 'vendors',
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
+    // splitChunks: {
+    //   // include all types of chunks
+    //   chunks: 'all',
+    //   // chunks: 'async',
+    //   cacheGroups: {
+    //     defaultVendors: {
+    //       // test: /[\\/]node_modules[\\/]/,
+    //       // reuseExistingChunk: true,
+    //       priority: -10,
+    //       test: /[\\/]node_modules[\\/]/,
+    //       // name: 'vendors',
+    //     },
+    //     default: {
+    //       minChunks: 2,
+    //       priority: -20,
+    //       reuseExistingChunk: true,
+    //     },
+    //   },
+    // },
 
     // TODO: Splitting Chunks
     // currently doesnt work on Webpack 5
@@ -117,46 +118,46 @@ module.exports = {
     //     },
     //   },
     // },
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-        // Use multi-process parallel running to improve the build speed
-        // Default number of concurrent runs: os.cpus().length - 1
-        parallel: true,
-        // Enable file caching
-        // cache: true,
-        terserOptions: {
-          parse: {
-            // we want terser to parse ecma 8 code. However, we don't want it
-            // to apply any minfication steps that turns valid ecma 5 code
-            // into invalid ecma 5 code. This is why the 'compress' and 'output'
-            // sections only apply transformations that are ecma 5 safe
-            // https://github.com/facebook/create-react-app/pull/4234
-            ecma: 8,
-          },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            // Disabled because of an issue with Uglify breaking seemingly valid code:
-            // https://github.com/facebook/create-react-app/issues/2376
-            // Pending further investigation:
-            // https://github.com/mishoo/UglifyJS2/issues/2011
-            comparisons: false,
-            // drop_console: true,
-          },
-          mangle: {
-            safari10: true,
-          },
-          output: {
-            ecma: 5,
-            comments: false,
-            // Turned on because emoji and regex is not minified properly using default
-            // https://github.com/facebook/create-react-app/issues/2488
-            ascii_only: true,
-          },
-        },
-      }),
-    ],
+    // minimizer: [
+    //   new TerserPlugin({
+    //     extractComments: false,
+    //     // Use multi-process parallel running to improve the build speed
+    //     // Default number of concurrent runs: os.cpus().length - 1
+    //     parallel: true,
+    //     // Enable file caching
+    //     // cache: true,
+    //     terserOptions: {
+    //       parse: {
+    //         // we want terser to parse ecma 8 code. However, we don't want it
+    //         // to apply any minfication steps that turns valid ecma 5 code
+    //         // into invalid ecma 5 code. This is why the 'compress' and 'output'
+    //         // sections only apply transformations that are ecma 5 safe
+    //         // https://github.com/facebook/create-react-app/pull/4234
+    //         ecma: 8,
+    //       },
+    //       compress: {
+    //         ecma: 5,
+    //         warnings: false,
+    //         // Disabled because of an issue with Uglify breaking seemingly valid code:
+    //         // https://github.com/facebook/create-react-app/issues/2376
+    //         // Pending further investigation:
+    //         // https://github.com/mishoo/UglifyJS2/issues/2011
+    //         comparisons: false,
+    //         // drop_console: true,
+    //       },
+    //       mangle: {
+    //         safari10: true,
+    //       },
+    //       output: {
+    //         ecma: 5,
+    //         comments: false,
+    //         // Turned on because emoji and regex is not minified properly using default
+    //         // https://github.com/facebook/create-react-app/issues/2488
+    //         ascii_only: true,
+    //       },
+    //     },
+    //   }),
+    // ],
   },
 
   plugins: [
