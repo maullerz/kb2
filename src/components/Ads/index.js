c3252ac22ea02c16cc4b7a7a918a9b627bf996ab
maullerz <maullerz@gmail.com>
2022-03-02 18:56:03 +0300

    Disabled ads

--- src/components/Ads/index.js
+++ src/components/Ads/index.js
@@ -31,38 +31,40 @@ function getSlot(type) {
 }
 
 const Ads = ({ type }) => {
-  const slot = getSlot(type)
-  if (!slot) {
-    console.error('undefined SLOT:', type)
-  }
+  return null
 
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'development') {
-      (window.adsbygoogle = window.adsbygoogle || []).push({})
-    }
-  }, [])
+  // const slot = getSlot(type)
+  // if (!slot) {
+  //   console.error('undefined SLOT:', type)
+  // }
 
-  if (process.env.NODE_ENV === 'development') {
-    return (
-      <Container>
-        <ins
-          style={responsiveStyle}
-          className='background'
-        />
-      </Container>
-    )
-  }
+  // useEffect(() => {
+  //   if (process.env.NODE_ENV !== 'development') {
+  //     (window.adsbygoogle = window.adsbygoogle || []).push({})
+  //   }
+  // }, [])
 
-  return (
-    <Container>
-      <ins
-        style={responsiveStyle}
-        className='adsbygoogle'
-        data-ad-client='ca-pub-3299420347078208'
-        data-ad-slot={slot}
-      />
-    </Container>
-  )
+  // if (process.env.NODE_ENV === 'development') {
+  //   return (
+  //     <Container>
+  //       <ins
+  //         style={responsiveStyle}
+  //         className='background'
+  //       />
+  //     </Container>
+  //   )
+  // }
+
+  // return (
+  //   <Container>
+  //     <ins
+  //       style={responsiveStyle}
+  //       className='adsbygoogle'
+  //       data-ad-client='ca-pub-3299420347078208'
+  //       data-ad-slot={slot}
+  //     />
+  //   </Container>
+  // )
 }
 
 export default Ads
