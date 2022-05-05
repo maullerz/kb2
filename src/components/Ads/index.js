fe29bdf1aa5d345a9cffbbe4c9a398f58f2af707
maullerz <maullerz@gmail.com>
2022-05-05 19:17:00 +0300

    Ads is On

--- src/components/Ads/index.js
+++ src/components/Ads/index.js
@@ -32,40 +32,41 @@ function getSlot(type) {
 }
 
 const Ads = ({ type }) => {
-  return <div style={{ height: 10 }} />
+  // when Ads Off
+  // return <div style={{ height: 10 }} />
 
-  // const slot = getSlot(type)
-  // if (!slot) {
-  //   console.error('undefined SLOT:', type)
-  // }
+  const slot = getSlot(type)
+  if (!slot) {
+    console.error('undefined SLOT:', type)
+  }
 
-  // useEffect(() => {
-  //   if (process.env.NODE_ENV !== 'development') {
-  //     (window.adsbygoogle = window.adsbygoogle || []).push({})
-  //   }
-  // }, [])
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'development') {
+      (window.adsbygoogle = window.adsbygoogle || []).push({})
+    }
+  }, [])
 
-  // if (process.env.NODE_ENV === 'development') {
-  //   return (
-  //     <Container>
-  //       <ins
-  //         style={responsiveStyle}
-  //         className='background'
-  //       />
-  //     </Container>
-  //   )
-  // }
+  if (process.env.NODE_ENV === 'development') {
+    return (
+      <Container>
+        <ins
+          style={responsiveStyle}
+          className='background'
+        />
+      </Container>
+    )
+  }
 
-  // return (
-  //   <Container>
-  //     <ins
-  //       style={responsiveStyle}
-  //       className='adsbygoogle'
-  //       data-ad-client='ca-pub-3299420347078208'
-  //       data-ad-slot={slot}
-  //     />
-  //   </Container>
-  // )
+  return (
+    <Container>
+      <ins
+        style={responsiveStyle}
+        className='adsbygoogle'
+        data-ad-client='ca-pub-3299420347078208'
+        data-ad-slot={slot}
+      />
+    </Container>
+  )
 }
 
 export default Ads
