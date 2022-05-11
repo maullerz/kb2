11457ec082f02ec87334e1d0c3e370830b0029ec
maullerz <maullerz@gmail.com>
2022-05-11 09:31:46 +0300

    SDE Update 2022-05-10

---
+++ src/modules/industry/api/IndustryService.js
@@ -0,0 +1,18 @@
+import BaseAPI from 'api/BaseAPI'
+
+const esiPath = 'https://esi.evetech.net/latest'
+const src = 'datasource=tranquility'
+
+class IndustryService extends BaseAPI {
+
+  getSomething() {
+    const esiUrl = `${esiPath}/...`
+    return this.call({
+      method: 'get',
+      url: esiUrl,
+    })
+  }
+
+}
+
+export default new IndustryService()
---
+++ src/modules/industry/pages/Blueprints/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+import { Root } from './styles'
+
+const Blueprints = () => {
+
+
+  return (
+    <Root>
+      .
+    </Root>
+  )
+}
+
+export default Blueprints
